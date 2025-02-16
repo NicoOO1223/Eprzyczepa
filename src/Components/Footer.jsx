b9e670a24f7d7c1bd13443406711f7fb7c1cdb1b
NicoOO1223 <nicosw.biz@gmail.com>
2025-02-16 23:01:07 +0100

    image paths

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -24,15 +24,15 @@ function Footer() {
           <h2>Skontaktuj się z nami!</h2>
           <div className="contact-details">
             <div className="contact-item">
-              <img src='/Img/Loc-r.png' alt="Location Icon" />
+              <img src={`${process.env.PUBLIC_URL}/images/Loc-r.png`} alt="Location Icon" />
               <p><strong>Address:</strong> 123 Street Name, City, Country</p>
             </div>
             <div className="contact-item">
-              <img src='/Img/Mail-b.png' alt="Email Icon" />
+              <img src={`${process.env.PUBLIC_URL}/images/Mail-b.png`} alt="Email Icon" />
               <p><strong>Email:</strong> example@email.com</p>
             </div>
             <div className="contact-item">
-              <img src='/Img/Phone-g.png' alt="Phone Icon" />
+              <img src={`${process.env.PUBLIC_URL}/images/Phone-g.png`} alt="Phone Icon" />
               <p><strong>Phone:</strong> +48 123 456 789</p>
             </div>
           </div>
--- src/Components/Oferta.jsx
+++ src/Components/Oferta.jsx
@@ -13,22 +13,22 @@ function Oferta() {
       <div className="items-container">
         
   <div className="item">
-  <img src='/images/kalendarz-b.png'></img>
+  <img src={`${process.env.PUBLIC_URL}/images/kalendarz-b.png`} alt="Calendar Icon" />
   <h2>Kiedy?</h2>
     <p>
     Przyczepy można wynająć przez całe wakacje, warto jednak zarezerwować z wyprzedzeniem, dostepnosc szybko maleje.
     </p>
   </div>
   <div className="item">
-  <img src='/images/hajs-g.png'></img>
+  <img src={`${process.env.PUBLIC_URL}/images/hajs-g.png`} alt="Calendar Icon" />
   <h2>Za ile?</h2>
     <p>
       Ceny zaczynają się już od 230 zł za dobę. 
       Zadzwoń do nas, a dobierzemy ofertę idealnie dopasowaną do Twoich potrzeb i budżetu!
     </p>
   </div>
-  <div className="item">
-  <img src='/Img/Loc.png'></img>
+  <div className="item">    
+  <img src={`${process.env.PUBLIC_URL}/images/Loc.png`} alt="Calendar Icon" />
   <h2>Gdzie?</h2>
     <p>
     Przyczepy znajdują się na Albatros 5,5,<br/> w otoczeniu licznych udogodnień jak sklepy, restauracje, szkółki oraz wypożyczalnie! </p>
