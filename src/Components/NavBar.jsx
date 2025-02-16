6482721e18bf8869371a9d341f88fcdb0dd554ff
NicoOO1223 <nicosw.biz@gmail.com>
2025-02-16 21:53:45 +0100

    img changes

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,36 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './NavBar.css';
 
 function NavBar() {
-    const scrollToSection = (id) => {
-        const section = document.getElementById(id);
-        if (section) {
-            section.scrollIntoView({ behavior: 'smooth'});
-        }
+  // State to store whether the page has been scrolled down
+  const [scrolled, setScrolled] = useState(false);
+
+  // Function to handle scroll event
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setScrolled(true); // Set to true when scrolled more than 50px
+    } else {
+      setScrolled(false); // Reset to false when scroll is back to the top
+    }
+  };
+
+  // Add scroll event listener when component mounts and clean up when it unmounts
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
     };
+  }, []);
+
+  // Function to scroll to a specific section
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
-    <nav className='navbar'>
-        <div className='navbar-container'>
-            <ul className='nav-menu'>
-                <li className='nav-item' onClick={() => scrollToSection('start')}>
-                    Start
-                </li>
-                <li className='nav-item' onClick={() => scrollToSection('oferta')}>
-                    Oferta
-                </li>
-                <li className='nav-item' onClick={() => scrollToSection('przyczepy')}>
-                    Przyczepy
-                </li>
-                <li className='nav-item' onClick={() => scrollToSection('cennik')}>
-                    Kontakt
-                </li>
-                <li className='nav-item'>
-                    Regulamin
-                </li>
-            </ul>
-        </div>
+    <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
+      <div className='navbar-container'>
+        <ul className='nav-menu'>
+          <li className='nav-item' onClick={() => scrollToSection('start')}>
+            Start
+          </li>
+          <li className='nav-item' onClick={() => scrollToSection('oferta')}>
+            Oferta
+          </li>
+          <li className='nav-item' onClick={() => scrollToSection('przyczepy')}>
+            Przyczepy
+          </li>
+          <li className='nav-item' onClick={() => scrollToSection('cennik')}>
+            Kontakt
+          </li>
+          <li className='nav-item'>
+            Regulamin
+          </li>
+        </ul>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
